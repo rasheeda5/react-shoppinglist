187bfd2a02ceeab8374497810c79d470390484cf
Rasheeda Abbas <rasheedaabbas@gmail.com>
2021-03-23 17:56:27 +0500

    update

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, FlatList} from 'react-native';
+import {View, Text, StyleSheet, FlatList, Alert} from 'react-native';
 import Header from './components/Header';
 import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
 import {v4 as uuidv4} from 'uuid';
 
 const App = () => {
@@ -24,15 +25,29 @@ const App = () => {
     },
   ]);
 
+  var count = 4;
+
   const deleteItem = id => {
     setItems(prevItems => {
       return prevItems.filter(item => item.id != id);
     });
   };
 
+  const addItem = text => {
+    if (!text) {
+      Alert.alert('Error', 'Please enter an item', [{text: 'Ok yaar'}]);
+    } else {
+      count++;
+      setItems(prevItems => {
+        return [{id: count, text}, ...prevItems];
+      });
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Header />
+      <AddItem addItem={addItem} />
       <FlatList
         data={items}
         renderItem={({item}) => (
